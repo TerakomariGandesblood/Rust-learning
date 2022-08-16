827f3361b8cf429393da0ca8471a9a84e9952baa
Terakomari <im.terakomari@icloud.com>
2022-08-16 13:53:59 +0800

    feat: reread chapter09

--- chapter/chapter09/src/bin/9_2.rs
+++ chapter/chapter09/src/bin/9_2.rs
@@ -14,13 +14,13 @@ fn main() {
         },
     };
 
-    // 如果 Result 值是成员 Ok, unwrap 会返回 Ok 中的值; 如果 Result 是成员 Err, unwrap 会调用 panic!
+    // 如果 Result 值是成员 Ok，unwrap 会返回 Ok 中的值；如果 Result 是成员 Err，unwrap 会调用 panic!
     let _file = File::open("hello.txt").unwrap();
-    // 和 unwrap() 类似, 可以选择错误信息, unwrap() 使用默认的 panic! 信息
+    // 和 unwrap() 类似，可以选择错误信息，unwrap() 使用默认的 panic! 信息
     let _file = File::open("hello.txt").expect("Failed to open hello.txt");
 }
 
-// 传播 (propagating) 错误
+// 传播（propagating）错误
 fn _read_username_from_file() -> Result<String, io::Error> {
     let mut file = match File::open("hello.txt") {
         Ok(file) => file,
@@ -35,10 +35,10 @@ fn _read_username_from_file() -> Result<String, io::Error> {
     }
 }
 
-// 简写: ? 运算符
-// 如果 Result 的值是 Ok, 这个表达式将会返回 Ok 中的值而程序将继续执行
-// 如果值是 Err, Err 中的值将作为整个函数的返回值
-// ? 会调用 from(), 定义于标准库的 From trait 中, 用于转换错误类型
+// 简写：? 运算符
+// 如果 Result 的值是 Ok，这个表达式将会返回 Ok 中的值而程序将继续执行
+// 如果值是 Err，Err 中的值将作为整个函数的返回值
+// ? 会调用 from()，定义于标准库的 From trait 中，用于转换错误类型
 fn _read_username_from_file2() -> Result<String, io::Error> {
     let mut file = File::open("hello.txt")?;
     let mut username = String::new();
@@ -63,10 +63,13 @@ fn _last_char_of_first_line(text: &str) -> Option<char> {
 }
 
 // TODO 第十七章
-// Box<dyn Error> 类型是一个 trait 对象 (trait object)
+// Box<dyn Error> 类型是一个 trait 对象（trait object）
 // 可以理解为任何类型的错误
 // 如果 main 返回 Ok(()) 可执行程序会以 0 值退出, 而如果 main 返回 Err 值则会以非零值退出
 // fn main() -> Result<(), Box<dyn Error>> {
 //     let f = File::open("hello.txt")?;
 //     Ok(())
 // }
+
+// main 函数也可以返回任何实现了 std::process::Termination trait 的类型
+// Termination trait 是一个不稳定功能（unstable feature）
