9100bb36c3666fa435e153ca8ff9b6fd4518f0ae
Terakomari <im.terakomari@icloud.com>
2022-10-04 21:22:04 +0800

    feat: reading chapter15

---
+++ chapter/chapter15/src/bin/15_3.rs
@@ -0,0 +1,26 @@
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    // 引用计数（reference counting）智能指针
+    // TODO 第十六章
+    // 注意 Rc<T> 只能用于单线程场景
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+
+    // 每次调用 Rc::clone，引用计数都会增加
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating a = {}", Rc::strong_count(&a));
+    }
+    println!("count after creating a = {}", Rc::strong_count(&a));
+}
