046cc87fcd2f71ebf6d2645b616ba53691c13a28
Terakomari <im.terakomari@icloud.com>
2022-10-10 14:12:38 +0800

    feat: finish chapter20

---
+++ chapter/chapter20/src/lib.rs
@@ -0,0 +1,101 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    // 传入的闭包不返回任何值
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+impl ThreadPool {
+    /// 创建线程池
+    ///
+    /// 线程池中线程的数量
+    ///
+    /// # Panics
+    ///
+    /// `new` 函数在 size 为 0 时会 panic
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        // 预先分配空间
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        // 由'static 限定的类型不能包含非static的引用类型
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            // 如果互斥器处于一种叫做被污染（poisoned）的状态时获取锁可能会失败，这可能发生于其他线程在持有锁时 panic 了且没有释放锁
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            };
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
--- chapter/chapter20/src/main.rs
+++ chapter/chapter20/src/main.rs
@@ -2,20 +2,25 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+use chapter20::ThreadPool;
 
 fn main() {
     // 连接到监听端口被称为 “绑定到一个端口”（“binding to a port”）
     // 绑定可能不成功，如端口已被占用
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let thread_pool = ThreadPool::new(4);
 
     // incoming 提供了一系列的流（更准确的说是 TcpStream 类型的流）
     // 流（stream）代表一个客户端和服务端之间打开的连接
     // 连接（connection）代表客户端连接服务端、服务端生成响应以及服务端关闭连接的全部请求 / 响应过程
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         // 连接可能不成功，如系统限制同时打开的连接数
         let stream = stream.unwrap();
-        handle_connection(stream);
         // 当 stream 在循环的结尾离开作用域并被丢弃，其连接将被关闭
+        thread_pool.execute(|| handle_connection(stream));
     }
 }
 
@@ -25,24 +30,32 @@ fn handle_connection(mut stream: TcpStream) {
     assert!(n <= 1024);
 
     // from_utf8_lossy 使用 �，U+FFFD REPLACEMENT CHARACTER，来代替无效序列
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-        let n = stream.write(response.as_bytes()).unwrap();
-        assert_eq!(n, response.len());
-
-        // flush 会等待并阻塞程序执行直到所有字节都被写入连接中
-        // TcpStream 包含一个内部缓冲区来最小化对底层操作系统的调用
-        stream.flush().unwrap();
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(100));
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        todo!();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    let n = stream.write(response.as_bytes()).unwrap();
+    assert_eq!(n, response.len());
+
+    // flush 会等待并阻塞程序执行直到所有字节都被写入连接中
+    // TcpStream 包含一个内部缓冲区来最小化对底层操作系统的调用
+    stream.flush().unwrap();
 }
