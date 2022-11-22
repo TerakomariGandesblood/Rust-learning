af57a2268004728dcec562d24e4b862100c3d9f5
Terakomari <im.terakomari@icloud.com>
2022-11-22 23:38:23 +0800

    feat: reading cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,11 @@
 // CLI (command line interface)
 
-use std::path::PathBuf;
+use std::{fs, path::PathBuf, thread, time::Duration};
 
+use anyhow::{Context, Ok, Result};
 use clap::Parser;
+use indicatif::ProgressBar;
+use tracing::{debug, info, trace};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -13,7 +16,49 @@ struct Cli {
     path: PathBuf,
 }
 
-fn main() {
+fn find_matches(content: &str, pattern: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    // 应该在 main 函数中使用，遇到错误时，将打印错误信息并退出
     let args = Cli::parse();
-    println!("{} {}", args.pattern, args.path.to_str().unwrap());
+
+    let pb = ProgressBar::new(5);
+    for i in 0..5 {
+        thread::sleep(Duration::from_millis(200));
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+
+    tracing_subscriber::fmt::init();
+    let number_of_yaks = 3;
+    info!("preparing to shave yaks: {}", number_of_yaks);
+
+    debug!("debug");
+    trace!("trace");
+
+    // TODO use https://doc.rust-lang.org/std/io/struct.BufReader.html#examples
+    let content = fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", &args.path.to_str().unwrap()))?;
+
+    find_matches(&content, &args.pattern);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_a_match() {
+        find_matches("lorem ipsum\ndolor sit amet", "lorem");
+        // assert_eq!()
+    }
 }
