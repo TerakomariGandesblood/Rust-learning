beee2041051856f5646009b830378b234e588120
Terakomari <im.terakomari@icloud.com>
2022-12-01 00:19:12 +0800

    feat: add some comment

--- trpl/chapter03/src/bin/3_1.rs
+++ trpl/chapter03/src/bin/3_1.rs
@@ -1,4 +1,6 @@
 // 常量（constants）必须注明类型，只能被设置为常量表达式
+// NOTE https://blog.rust-lang.org/2017/04/27/Rust-1.17.html#whats-in-1170-stable
+// const 和 static 的引用隐含 'static
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
